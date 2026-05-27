9931ff1ae282aae46c884a41e05701df0d018484
namdq <namdq97@gmail.com>
2026-05-27 09:10:09 +0700

    Add new SVG icons for debit card, healthcare, loan, and verification
    - Added ic_debit_card.svg with a design representing a debit card.
    - Added ic_healthcare.svg featuring a healthcare-related graphic.
    - Introduced ic_loan.svg depicting a loan-related icon.
    - Created ic_verify.svg illustrating a verification symbol.

--- apps/sk-106/public/assets/icons/index.ts
+++ apps/sk-106/public/assets/icons/index.ts
@@ -3,6 +3,12 @@ export const ICONS = {
   ic_vcb: '/assets/icons/logo_vcb.svg',
   ic_refresh: '/assets/icons/ic_refresh.svg',
   ic_logo_full: '/assets/icons/logo_full.svg',
+  ic_verify: '/assets/icons/ic_verify.svg',
+  ic_loan: '/assets/icons/ic_loan.svg',
+  ic_healthcare: '/assets/icons/ic_healthcare.svg',
+  ic_credit_card: '/assets/icons/ic_credit_card.svg',
+  ic_debit_card: '/assets/icons/ic_debit_card.svg',
+  ic_check_circle_fill: '/assets/icons/ic_check_circle_fill.svg',
 } as const;
 
 export type IconName = keyof typeof ICONS;
